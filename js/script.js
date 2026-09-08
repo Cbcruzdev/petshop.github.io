b78bbefbb9ee29587535e08190c3121b0e7a1339
Cbcruzdev <cbcruz.dev@gmail.com>
2026-09-08 00:25:39 -0300

    Add console message on page load and alerts

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+// Mostra uma mensagem no console quando o site é carregado
+window.addEventListener("load", function() {
+    console.log("Amigo Pet carregado com sucesso!");
+});
+
+// Mostra a data e o horário atual na página
 function mostrarData() {
     const elementoData = document.getElementById("dataAtual");
 
@@ -8,12 +14,15 @@ function mostrarData() {
     }
 }
 
+// Atualiza a data e o horário automaticamente
 setInterval(mostrarData, 1000);
 
+// Exibe uma mensagem quando o cadastro é enviado
 function confirmarCadastro() {
     alert("Cadastro realizado com sucesso!");
 }
 
+// Exibe uma mensagem quando o agendamento é enviado
 function confirmarAgendamento() {
     alert("Agendamento realizado com sucesso!");
 }
